399703e60d0da53ffbb74c0eec18d11c3f918731
Hong Duyen NP <111115846+hduyen01@users.noreply.github.com>
2023-11-30 07:05:54 +0700

    Add files via upload

--- FRONT_END/home_page/homepage.js
+++ FRONT_END/home_page/homepage.js
@@ -101,3 +101,88 @@ function scrollProducts_2(n) {
 }
 // Initialize the product display
 scrollProducts_2(0);
+
+
+// Slider Container //
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
+const slider = document.querySelector('.slider');
+
+function showSlide(index) {
+  if (index < 0) {
+    slideIndex = totalSlides - 1;
+  } else if (index >= totalSlides) {
+    slideIndex = 0;
+  }
+
+  const offset = -slideIndex * 100;
+  slider.style.transform = `translateX(${offset}%)`;
+}
+
+function prevSlide() {
+  showSlide(--slideIndex);
+}
+
+function nextSlide() {
+  showSlide(++slideIndex);
+}
+
+// Automatic slide change
+setInterval(() => {
+  nextSlide();
+}, 5000); // Change slide every 5 seconds
+
+// Initial slide display
+showSlide(slideIndex);
+
+
+//sp hnay
+let productIndex_3 = 0;
+let displayCount_3 = 3;
+
+function scrollProducts_3(n) {
+    let products_3 = document.getElementsByClassName('product-card');
+    productIndex_3 += n;
+
+    // Wrap around the products
+    if (productIndex_3 < 0) {
+        productIndex_3 = products_3.length - displayCount_3;
+    } else if (productIndex_3 > products_3.length - displayCount_3) {
+        productIndex_3 = 0;
+    }
+
+    // Hide all products
+    for (let i = 0; i < products_3.length; i++) {
+        products_3[i].style.display = 'none';
+    }
+
+    // Show the current products
+    for (let i = productIndex_3; i < productIndex_3 + displayCount_3; i++) {
+        products_3[i].style.display = 'block';
+    }
+}
+
+// Initialize the product display
+scrollProducts_3(0);
+
+// Automatic product change
+setInterval(() => {
+  scrollProducts_3(1);
+}, 3000); // Change product every 3 seconds
+
+// Navigation buttons
+function prevSlide_3() {
+  scrollProducts_3(-displayCount_3);
+}
+
+function nextSlide_3() {
+  scrollProducts_3(displayCount_3);
+}
+
+// Add event listeners to the buttons
+document.querySelector('.prev').addEventListener('click', prevSlide_3);
+document.querySelector('.next').addEventListener('click', nextSlide_3);
+
+
+
