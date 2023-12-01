971d17374bfc0309484fc14c04f9403a06db8056
NhatSinh2002 <nhatsinh2002.pt@gmail.com>
2023-12-01 16:33:25 +0700

    commit

--- Sinh/JS/script.js
+++ Sinh/JS/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $(".flexnav").flexNav();
-});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
