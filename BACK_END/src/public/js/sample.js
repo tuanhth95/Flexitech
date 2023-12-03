99d9e8f5eefc6023200d6bc67ed59fc939bc3c9b
Phatnhoy <thiephongphuc56@gmail.com>
2023-12-03 23:51:44 +0700

    filter-done

--- BACK_END/src/public/js/sample.js
+++ BACK_END/src/public/js/sample.js
@@ -3,11 +3,16 @@ console.log("Success");
 function getHomepage(){
     console.log("Success");
 }
-// var li = document.getElementsByClassName("product-card");
-// var div = li[0].getElementsByTagName("div");
-// var span = div[3].getElementsByTagName("span");
-// console.log(span[0].textContent);
+
 var pro_li = document.getElementsByClassName("product-card");
+for (i = 0; i < pro_li.length; i++) {
+    pro_li[i].setAttribute("logo-filter", "yes");
+    pro_li[i].setAttribute("price-filter", "yes");
+    pro_li[i].setAttribute("status-filter", "yes");
+    pro_li[i].setAttribute("checkbox-filter", "yes");
+}
+
+
 // filter theo logo
 var logofilter = document.getElementsByClassName("product_logo_container");
 var li = document.getElementsByClassName("product_logo");
@@ -22,9 +27,11 @@ function selectLogo(){
             li[i].style.backgroundColor = "rgba(0,0,0,0)";
             li[i].setAttribute("checked", "unchecked");
         }
-        var pro_li = document.getElementsByClassName("product-card");
         for (i = 0; i < pro_li.length; i++) {
-                pro_li[i].style.display = "";
+            if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                pro_li[i].style.display = "";   
+            }
+                pro_li[i].setAttribute("logo-filter", "yes");
         }
         this.style.backgroundColor = "rgba(0,0,0,0.5)";
         this.setAttribute("checked", "checked");
@@ -32,16 +39,18 @@ function selectLogo(){
             var pro_brand = pro_li[i].getAttribute("brand");
             if (pro_brand != this.getAttribute("id")){
                 pro_li[i].style.display = "none";
+                pro_li[i].setAttribute("logo-filter", "no");
             }
         }
     }
     else {
         this.style.backgroundColor = "rgba(0,0,0,0)";
         this.setAttribute("checked", "unchecked")
-        var product = document.getElementsByClassName("product_pages");
-        var pro_li = document.getElementsByClassName("product-card");
         for (i = 0; i < pro_li.length; i++) {
-                pro_li[i].style.display = "";
+            if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                pro_li[i].style.display = "";   
+            }
+                pro_li[i].setAttribute("logo-filter", "yes");
         }
     }
 }
@@ -54,64 +63,81 @@ function selectPrices(){
     for (i = 0; i < prices.length; i++) {
         if (prices[i].selected == true) {
             var pricevalue = prices[i].getAttribute("value");
-            var product = document.getElementsByClassName("product_pages");
             if (pricevalue=="dart") {
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("price-filter", "yes");
                 }        
             }
             else if (pricevalue=="price1") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("price-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var div = pro_li[i].getElementsByTagName("div");
                     var span = div[3].getElementsByTagName("span");
                     if ((span[0].textContent) > 100) {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("price-filter", "no");
                     }
                 }  
             }
             else if (pricevalue=="price2") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("price-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var div = pro_li[i].getElementsByTagName("div");
                     var span = div[3].getElementsByTagName("span");
                     if ((span[0].textContent) > 200 || (span[0].textContent) < 100) {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("price-filter", "no");
                     }
                 }  
             }
             else if (pricevalue=="price3") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("price-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var div = pro_li[i].getElementsByTagName("div");
                     var span = div[3].getElementsByTagName("span");
                     if ((span[0].textContent) > 300 || (span[0].textContent) < 200) {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("price-filter", "no");
                     }
                 }  
             }
             else if (pricevalue=="price4") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("status-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("price-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var div = pro_li[i].getElementsByTagName("div");
                     var span = div[3].getElementsByTagName("span");
                     if ((span[0].textContent) > 400 || (span[0].textContent) < 300) {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("price-filter", "no");
                     }
                 }  
             }
         }
     }
 }
-
 
 
 
@@ -126,28 +152,39 @@ function selectStatus(){
             var statusvalue = statuss[i].getAttribute("value");
             if (statusvalue=="dart") {
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("status-filter", "yes");
                 }        
             }
             else if (statusvalue=="status1") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("status-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var pro_status = pro_li[i].getAttribute("status");
                     if (pro_status != "New") {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("status-filter", "no");
                     }
                 }  
             }
             else if (statusvalue=="status2") { 
                 for (i = 0; i < pro_li.length; i++) {
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("checkbox-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("status-filter", "yes");
                 }   
                 for (i = 0; i < pro_li.length; i++) {
                     var pro_status = pro_li[i].getAttribute("status");
                     if (pro_status != "Used") {
                         pro_li[i].style.display = "none";
+                        pro_li[i].setAttribute("status-filter", "no");
                     }
                 } 
             }
@@ -155,12 +192,11 @@ function selectStatus(){
     }
 }
 
+
 //filter theo checkbox
 var checkbox = document.querySelectorAll("input[type=checkbox]");
-console.log(checkbox);
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].addEventListener("change", selectCheckbox);
-    console.log(checkbox[i].checked);
 }   
 function selectCheckbox(){
     var count=0;
@@ -169,23 +205,31 @@ function selectCheckbox(){
     }   
     if (count == 0) {
         for (i = 0; i < pro_li.length; i++) {
-            pro_li[i].style.display = "";
+            if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("status-filter")=="yes"){
+                pro_li[i].style.display = "";   
+            }
+            pro_li[i].setAttribute("checkbox-filter", "yes");
         }      
     }
     else if (count==1) {   
         if (this.checked) {
             for (i = 0; i < pro_li.length; i++) {
                 pro_li[i].style.display = "none";
+                pro_li[i].setAttribute("checkbox-filter", "no");
             }   
             for (i = 0; i < pro_li.length; i++) {
                 if (pro_li[i].getAttribute("kind")==this.getAttribute("id")){
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("status-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("checkbox-filter", "yes");
                 }    
             }   
         } else {    
             for (i = 0; i < pro_li.length; i++) {
                 if (pro_li[i].getAttribute("kind")==this.getAttribute("id")){
                     pro_li[i].style.display = "none";
+                    pro_li[i].setAttribute("checkbox-filter", "no");
                 }   
             } 
         }  
@@ -194,13 +238,17 @@ function selectCheckbox(){
         if (this.checked) {
             for (i = 0; i < pro_li.length; i++) {
                 if (pro_li[i].getAttribute("kind")==this.getAttribute("id")){
-                    pro_li[i].style.display = "";
+                    if (pro_li[i].getAttribute("price-filter")=="yes" && pro_li[i].getAttribute("logo-filter")=="yes" && pro_li[i].getAttribute("status-filter")=="yes"){
+                        pro_li[i].style.display = "";   
+                    }
+                    pro_li[i].setAttribute("checkbox-filter", "yes");
                 }    
             }   
         } else {
             for (i = 0; i < pro_li.length; i++) {
                 if (pro_li[i].getAttribute("kind")==this.getAttribute("id")){
                     pro_li[i].style.display = "none";
+                    pro_li[i].setAttribute("checkbox-filter", "no");
                 }   
             } 
         }  
