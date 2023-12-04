fb76a64705240737642fbc1314e59618ed8757a0
tuanhth95 <minhtuanhth811@gmail.com>
2023-12-05 02:00:41 +0700

    login js process hide password

---
+++ FRONT_END/login/login.js
@@ -0,0 +1,17 @@
+let hide = document.querySelector('.password div:has(i)');
+hide.addEventListener('click', switchHide);
+
+function switchHide() {
+    console.log('triggered');
+    if(document.querySelector('.password input').getAttribute('type') == "text"){
+        document.querySelector('.password input').setAttribute('type', 'password');
+        document.querySelector('.password i:first-of-type').setAttribute('style', 'display:none');
+        document.querySelector('.password i:last-of-type').setAttribute('style', 'display:inline');
+    }
+        
+    else{
+        document.querySelector('.password input').setAttribute('type', 'text');
+        document.querySelector('.password i:first-of-type').setAttribute('style', 'display:inline');
+        document.querySelector('.password i:last-of-type').setAttribute('style', 'display:none');
+    } 
+}
