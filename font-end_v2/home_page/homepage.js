5f0bb5c3179d8fc06e8488b7b371829d18c652b2
NhatSinh2002 <nhatsinh2002.pt@gmail.com>
2023-12-06 00:04:33 +0700

    commit

--- font-end_v2/home_page/homepage.js
+++ font-end_v2/home_page/homepage.js
@@ -292,3 +292,14 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
